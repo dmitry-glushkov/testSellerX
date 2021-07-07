561a2cfe37feb92d27b3bd34b407c74c9fd8683b
dmitry-glushkov <glushkov.dmitry.fn@yandex.ru>
2021-07-07 21:39:41 +0300

    init handlers

--- internal/apiserver/handlers.go
+++ internal/apiserver/handlers.go
@@ -1,1 +1,33 @@
 package apiserver
+
+import "net/http"
+
+func (s *server) handleUserCreate() http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+	})
+}
+
+func (s *server) handleChatCreate() http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+	})
+}
+
+func (s *server) handleSendMessage() http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+	})
+}
+
+func (s *server) handleGetUserChats() http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+	})
+}
+
+func (s *server) handleGetChatMessages() http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+	})
+}
--- internal/apiserver/server.go
+++ internal/apiserver/server.go
@@ -9,3 +9,24 @@ type server struct {
 	router *mux.Router
 	logger *logrus.Logger
 }
+
+// Создание и насройка сервера
+func newServer() *server {
+	s := &server{
+		router: mux.NewRouter(),
+		logger: logrus.New(),
+	}
+
+	s.configureRouter()
+
+	return s
+}
+
+// Связывание хэндлеров и соответствующих им URL
+func (s *server) configureRouter() {
+	s.router.HandleFunc("/users/add", s.handleUserCreate()).Methods("POST")
+	s.router.HandleFunc("/chats/add", s.handleChatCreate()).Methods("POST")
+	s.router.HandleFunc("/messages/add", s.handleSendMessage()).Methods("POST")
+	s.router.HandleFunc("/chats/get", s.handleGetUserChats()).Methods("POST")
+	s.router.HandleFunc("/messages/get", s.handleGetChatMessages()).Methods("POST")
+}
